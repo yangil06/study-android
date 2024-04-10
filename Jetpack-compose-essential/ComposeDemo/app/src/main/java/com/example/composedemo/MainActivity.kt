f8de333d26fff87e6a2bb90c67ff30a1af2d2fec
han-gil <hangil@zeronecs.com>
2024-04-10 17:16:03 +0900

    ~chapter19 kotlin 및 compose 기초

--- Jetpack-compose-essential/ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
+++ Jetpack-compose-essential/ComposeDemo/app/src/main/java/com/example/composedemo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.composedemo
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.os.Bundle
+import android.provider.Contacts.SettingsColumns.KEY
 import android.view.Menu
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -21,6 +24,9 @@ import androidx.compose.ui.unit.sp
 import com.example.composedemo.ui.theme.ComposeDemoTheme
 
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.toUpperCase
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +59,55 @@ class MainActivity : ComponentActivity() {
         Toast.makeText(applicationContext, "", Toast.LENGTH_SHORT).show()
         //이 때는 Ctrl + Space 눌러서
         //applicatoinContext 도 잘 검색됨..
+
+        nullExceptionTest()
+        //and run Ctrl+F9, Shift+F10
+        //Shift+F9 Debug, Step F8, into F7
+        //Continue F9
+
+        //안전한 타입변환 연산자는
+        //캐스팅을 할 수 없으면 null을 반환한다.
+        val keyMgr = getSystemService(Context.KEYGUARD_SERVICE) as? KeyguardManager
+
+        //비트 연산자나 논리연산자에 대해서는 축약형 함수를 제공
+        /*
+        val int1 : Int
+        int1.inv()
+        int1.and()
+        int1.or
+        int1.xor()
+        int1.shl
+        int1.shr
+         */
+
+        //compose declarative, data-driven 버그에 취약하지 않도록
+        //컴포즈를 이용하면, 레이아웃을 빌드하는 접근 방식의 복잡성에 신경쓰지 않고
+        //사용자 인터페이스가 표시되는 방식을 선언함으로써 레이아웃을 만들 수 있다.
+        //프로그래머가 선언만 하면 레이아웃 배치, 제한, 렌더링 방법에 관한 모든
+        //복잡한 세부사항을 컴포즈가 자동으로 처리한다.
+
+        //컴포즈 도입 이전에 안드로이드 앱에는 앱 내 데이터의 현재 값을 확인하는 코드를
+        //작성해야 했다.
+        //예를 들면??
+        //데이터 변경사항을 자주 확인하는 코드, 사용자가 데이터 업데이트를 요청할 수 있는
+        //새로 고침 옵션을 제공하는 등..
+        //사용자 인터페이스 상태를 인관되게 유지하건, 토글 버튼 설정등이 적절하게 저장되었는지 확인해야 할때
+        //앱의 여러 영역에서 동일한 데이터 소스에 의존할 수록 이런 요구사항은 더욱 복잡해진다
+
+        //컴포즈는 State 기반 시스템을 제공해 이런 복잡성을 해결한다.
+        //데이터의 변경을 확인하는 코드를 넣지 않아도, 변경사항이 사용자 인터페이스에 자동으로
+        //반영된다. 상태에 접근하는 모든 사용자 인터페이스 컴포넌트는
+        //기본적으로 그 상태를 구독한다.
+/*
+        var sliderPosition by remember {mutableStateOf(20f)}
+        val handlePositionChange = {position : Float ->
+            sliderPosition = position
+        }
+ */
+        //이것을 예로 들면, HomeScreen 이 sliderPosition 을 구독하게
+        //된다는 것 같다.
+        //따라서 sliderPosition 이 변경되면,
+        //HomeScreen 의 Recomposition 이 일어난다는 것.
     }
 
     //Code Generation
@@ -178,7 +233,7 @@ fun myMethod() {
 /*
 안드로이드 스튜디오 안에서 안드로이드 앱을 빌드하면 중간 바이트코드 형식(DEX)으로
 컴파일된다.
-이후 기기에 애플리케이션이 로드되면 안드로이드 런타임은(ART) Ahead=of-Time
+이후 기기에 애플리케이션이 기기에 설치되면 안드로이드 런타임은(ART) Ahead=of-Time
 컴파일이라 불리는 프로세스를 통해 바이트코드를 기기의 프로세스가 요청하는
 기본 명령으로 변환한다. 이 형식을 실행 및 연결 가능한 형식
 (Executable and Linkable Format, ELF)이라 부른다.
@@ -187,3 +242,24 @@ fun myMethod() {
 이는 애플리케이션을 시작할 때마다 가상 머신안에서 바이트코드를 변환했던
 이전 안드로이드 구현에서의 JIT 컴파일 접근 방식과 대조된다.
 */
+
+fun nullExceptionTest() : String {
+    val username: String? = null
+    username?.length //null이면 수행 안되는게 safecall인가 보넹..
+    print(username?.length)
+
+    //let 함수를 호출하면 안전 호출 연산자를 사용했으므로
+    //변수에 null이 아닌 값이 할당되었을 때만 let 함수를 호출한다.
+    username?.let {
+        print(username.length)
+    }
+
+    //lateinit 초기화 지연
+    lateinit var myName: String
+//    print("My name is " + myName)
+    //throwing exception
+    //if (myName.isInitialized) { } <- 안되는디..
+
+    //elvis
+    return myName ?: "String is null"
+}
