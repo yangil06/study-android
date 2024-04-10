b6c8d7ad211d7a99ef2f024c816259c2fb03366b
han-gil <hangil@zeronecs.com>
2024-04-10 19:52:52 +0900

    Chapter20. state hoisting

--- Jetpack-compose-essential/StateExample/app/src/main/java/com/example/stateexample/MainActivity.kt
+++ Jetpack-compose-essential/StateExample/app/src/main/java/com/example/stateexample/MainActivity.kt
@@ -34,57 +34,30 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen() {
-    MyTextField()
-}
-
-@Composable
-fun MyTextField() {
     var textState by remember { mutableStateOf("") }
     val onTextChange = { text : String ->
         textState = text //setter(text) 동작
     }
+    MyTextField(textState, onTextChange)
+}
+
+//State hoisting
+//상태를 부모쪽으로 이동함을 의미
+//사용자가 입력한 텍스트가 함수 호출에 접근할 수 없기 때문에
+//그 형제 함수들에도 전달할 수 없고
+//다른 상태나 이벤트 핸들러도 함수로 전달할 수 없으므로 재사용성이 저하된다.
+//부모로 들어올리면 이 문제들이 일부 해결되나보다..--;;
+//어떻게 보면, 공통변수를 부모클래스로 끌어올려서 자식클래스들에서
+//공통적으로 이용할 수 있게 한 것과 별 다른게 없는 행위이다.
+//UI 값이라는 측면에서 좀 더 의미가 다를 순 있겠지만...
+@Composable
+fun MyTextField(textState: String, onTextChange : (String) -> Unit) {
     TextField(
         value = textState,//getter() 동작
         //onTextChange = onTextChange, ctrl 을 눌러서 들어가보면, onTextChange라는 파라미터는 없음
         onValueChange = onTextChange
     )
 }
-
-//unidirectional data flow 개념
-@Composable
-fun FunctionA() {
-    var switchState by remember { mutableStateOf(true) }
-    val onSwitchChange = { value : Boolean ->
-        switchState = value
-    }
-    FunctionB(
-        switchState = switchState,
-        onSwitchChange = onSwitchChange
-    )
-}
-
-@Composable
-fun FunctionB(switchState: Boolean, onSwitchChange : (Boolean) -> Unit ) {
-    Switch(
-        checked = switchState,
-        onCheckedChange = onSwitchChange
-    )
-}
-//이 프로세스에서의 핵심은 switchState에 할당된 값은 FunctionA에서만 직접 변경을 다루고
-//FunctionB에 의해 직접 업데이트되지 않는다는 점이다.
-//Switch설정은 FunctionB가 직접 on 위치에서 off 위치로 바꾸지 않는다.
-//대신 FunctionA의 이벤트 핸들러를 호출해 상태를 변경하고,
-//그 결과를 이용해 재구성함으로써 Switch를 새로운 윗치값으로 만든다.
-//규칙에 의해
-//이벤트는 Switch->FunctionB->FunctionA 에서 변경이 일어난다.
-//변경이 일어나서, switchState을 subscribe 한 FunctionA는
-// switchState = value 에 의해,
-// Compose Runtime 에 Observerable 함수를 트리거하게 되고
-// 그 트리거는, FunctionA를 트리거하여 recomposition 한다
-// 이 때 switchState 의 데이터, switchState.value, 즉 위젯에 필요한 데이터는
-// FunctionA -> FunctionB(switchState) -> Switch(switchState)
-// 로 bool 값이 상태의 getter에 의해
-//Switch 로 전달된다.
 
 @Preview(showBackground = true)
 @Composable
@@ -98,6 +71,5 @@ fun DefaultPreview() {
 fun showComponents() {
     Column {
         DemoScreen()
-        FunctionA()
     }
 }
